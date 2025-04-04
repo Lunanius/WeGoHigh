031758ef13e9cd5c8f3b16f6290b1aa495550709
sungmin010917 <dta0917@naver.com>
2025-04-04 16:09:17 +0900

    Add files via upload

--- App.js
+++ App.js
@@ -5,16 +5,17 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>We go high</h1>
-
-
-      </header>
-      <body>
-          <p>정보를 원하는 기사의 URL을<br/>입력해 보세요.</p>
-          <div className="Search">
-              <input id="search-input" placeholder="뉴스 URL을 입력하세요."/>
+          <div className="container">
+              <h1>We go high</h1>
+              <button className="login-button" type="button" onClick="location.href='login.js'">로그인</button>
           </div>
-      </body>
+          <p className="App-body">정보를 원하는 기사의 URL을<br/>입력해 보세요.</p>
+          <div className="Search">
+            <input id="search-input" placeholder="뉴스 URL을 입력하세요."/>
+        </div>
+        <p className="App-tail">로그인해서 전에 검색했던 내용을<br/>다시 확인하세요!</p>
+      </header>
+
     </div>
   );
 }
