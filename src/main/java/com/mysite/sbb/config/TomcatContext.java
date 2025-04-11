3108b6bea66e3d0b26db3c73006b3340cda5524c
leesn456 <leesn456@github.com>
2025-04-11 23:46:59 +0900

    Carpe diem

---
+++ src/main/java/com/mysite/sbb/config/TomcatContext.java
@@ -0,0 +1,21 @@
+package com.mysite.sbb.config;
+
+import org.apache.tomcat.util.http.Rfc6265CookieProcessor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.web.embedded.tomcat.TomcatContextCustomizer;
+
+public class TomcatContext {
+    @Configuration
+    public class CookieConfig {
+
+        @Bean
+        public TomcatContextCustomizer sameSiteCookieConfig() {
+            return context -> {
+                Rfc6265CookieProcessor cookieProcessor = new Rfc6265CookieProcessor();
+                cookieProcessor.setSameSiteCookies("Lax"); // ← 여기서 SameSite 설정!
+                context.setCookieProcessor(cookieProcessor);
+            };
+        }
+    }
+}
---
+++ src/main/java/com/mysite/sbb/fastapi/CalcController.java
@@ -0,0 +1,4 @@
+package com.mysite.sbb.fastapi;
+
+public class CalcController {
+}
